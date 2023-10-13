53adc6a6a1c5c7f19eb4aa5d8a8653c1fe509701
maade69 <70593890+maade69@users.noreply.github.com>
2023-10-13 17:25:06 +0300

    add PixelDisplayService permissions

--- device-common.mk
+++ device-common.mk
@@ -20,6 +20,10 @@ include device/google/zumapro/device.mk
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.carrierlock.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.carrierlock.xml
 
+# Displayservice
+PRODUCT_COPY_FILES += \
+    device/google/zumapro/system_ext-permissions-stock.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/system_ext-permissions-stock.xml
+
 # Android Verified Boot
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
