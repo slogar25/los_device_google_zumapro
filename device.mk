248f3538871a6602c6fc211b2c63915aebdce17d
Cyan_Hsieh <cyanhsieh@google.com>
2024-05-21 18:20:12 +0800

    Add manifest for VINTF target FCM level 202404
    Target FCM level 202404 deprecated the following HALs:
        android.hardware.graphics.mapper
    Target FCM level 202404 requires the newer version of the following HALs:
        android.hardware.radio.config
        android.hardware.radio.data
        android.hardware.radio.messaging
        android.hardware.radio.modem
        android.hardware.radio.network
        android.hardware.radio.sim
        android.hardware.radio.voice
    Add the previous version of these HALs into the customized product FCM
    before ready to remove them from device manifest
    
    Bug: 312876454
    Flag: EXEMPT NDK
    Change-Id: Ic9af791e7aa2027bf8d7ff9c39cf097323d7b68a

--- device.mk
+++ device.mk
@@ -398,8 +398,19 @@ PRODUCT_VENDOR_PROPERTIES += \
 PRODUCT_SHIPPING_API_LEVEL := $(SHIPPING_API_LEVEL)
 
 # Device Manifest, Device Compatibility Matrix for Treble
+#
+# Install product specific framework compatibility matrix
+# (TODO: b/169535506) This includes the FCM for system_ext and product partition.
+# It must be split into the FCM of each partition.
+ifeq ($(PRODUCT_SHIPPING_API_LEVEL),35)
+DEVICE_MANIFEST_FILE := \
+	device/google/zumapro/manifest_202404.xml
+DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/zumapro/device_framework_matrix_product_202404.xml
+else
 DEVICE_MANIFEST_FILE := \
 	device/google/zumapro/manifest.xml
+DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/zumapro/device_framework_matrix_product.xml
+endif
 
 ifneq (,$(filter aosp_%,$(TARGET_PRODUCT)))
 DEVICE_MANIFEST_FILE += \
@@ -1185,11 +1196,6 @@ include hardware/google/pixel/wifi_ext/device.mk
 
 # Battery Stats Viewer
 PRODUCT_PACKAGES_DEBUG += BatteryStatsViewer
-
-# Install product specific framework compatibility matrix
-# (TODO: b/169535506) This includes the FCM for system_ext and product partition.
-# It must be split into the FCM of each partition.
-DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/zumapro/device_framework_matrix_product.xml
 
 # Keymint configuration
 PRODUCT_COPY_FILES += \
