677ebcba0548093ccb6cb909925de54b57a86dca
Michael Bestas <mikeioannina@gmail.com>
2024-06-12 22:03:10 +0300

    Initial aam policy
    Change-Id: I81cf99339193df3ed5341fb34e338363cd63fa24

--- sepolicy/zumapro-sepolicy.mk
+++ sepolicy/zumapro-sepolicy.mk
@@ -10,6 +10,7 @@ BOARD_SEPOLICY_DIRS += \
 BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/googlebattery
 
 # sepolicy that are shared among devices using zumapro
+BOARD_SEPOLICY_DIRS += device/google/zumapro/sepolicy/aam
 BOARD_SEPOLICY_DIRS += device/google/zumapro/sepolicy/vendor
 BOARD_SEPOLICY_DIRS += device/google/zumapro/sepolicy/radio
 PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/zumapro/sepolicy/radio/private
