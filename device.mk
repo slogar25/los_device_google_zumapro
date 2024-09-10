147b6373f471c5ac0494c2127f7c34c736934437
Chia-Chi Teng <ccteng@google.com>
2024-09-10 00:42:02 +0000

    Deprecate preloaded_nanoapps.json
    Flag: EXEMPT resource only update
    Bug: 350102369
    Test: on device and presubmit
    Change-Id: I8056d46ef653b3cb6b498b02c5aee18ca22a53d0

--- device.mk
+++ device.mk
@@ -518,8 +518,6 @@ PRODUCT_COPY_FILES += \
 
 ## Enable the CHRE Daemon
 CHRE_USF_DAEMON_ENABLED := false
-PRODUCT_PACKAGES += \
-	preloaded_nanoapps.json
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
